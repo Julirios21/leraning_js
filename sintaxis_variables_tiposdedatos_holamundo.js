e21f1600bd32dfc77be22c1ba07f030d9774c0fd
Julirios21 <julirios21.jr@gmail.com>
2025-01-25 11:59:41 -0500

    file 00 added
    file 00 where i learned state variables, objects, arrays and functions

---
+++ sintaxis_variables_tiposdedatos_holamundo.js
@@ -0,0 +1,26 @@
+// https://www.javascript.com
+
+var strin = 0; //la mas comun forma
+let Hola_mundo = 'Hola mundo' // ta bien pero no tan chebere(mas privada)
+const constante = true //directamente es una constante
+
+let Nan = "Nan" //no es un numero
+let nulo = null //no tiene valor
+let indefinido = undefined //no esta definido
+
+let objeto = {} //objeto vacio
+let arreglo = [] //arreglo vacio
+
+let javascript = ""
+let funcion = function(){
+    javascript = "JavaScript!"
+} //funcion que guarda el string "JavaScript!" en la variable javascript
+funcion() //llamamos a la funcion
+
+/*
+que chere tener las teclas para los comentarios tan bien puestas
+*/
+
+console.log("¡Hola, JavaScript!")
+
+console.log(`${Hola_mundo}, estoy usando ${javascript}`) //imprime "Hola mundo, estoy usando JavaScript!" en la consola
